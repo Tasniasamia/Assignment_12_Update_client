748a7dc934ca6079479a07edc1c3a5556c36105e
tasniaarham <tasniaarhamarham@gmail.com>
2023-06-08 20:37:19 +0600

    add_class_done

--- src/Components/Pages/Dashboard/Add_Class/Add_Class.jsx
+++ src/Components/Pages/Dashboard/Add_Class/Add_Class.jsx
@@ -1,22 +1,60 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../Shared/AuthProvider/AuthProvider';
+import axios from 'axios';
 
 const Add_Class = () => {
     const {data}=useContext(AuthContext);
+    console.log("Tasnia")
+    const SubmitData=(event)=>{
+        event.preventDefault();
+const Class_name=event.target.class_name.value;
+const Class_image=event.target.Class_image.value;
+const Instructor_Name=event.target.Instructor_Name.value;
+const Instructor_Email=event.target.Useremail.value;
+const Available_seats=event.target.Available_seats.value;
+const Price=parseInt(event.target.price.value);
+const data={
+    Class_name:Class_name,
+    Class_image:Class_image,
+    Instructor_Name:Instructor_Name,
+    Instructor_Email:Instructor_Email,
+    Available_seats:Available_seats,
+    Price:Price,
+    status:"Pending",
+    Approved:"Approve",
+    Deny:"Deny"
+}
+
+
+console.log(data);
+
+axios.post('http://localhost:6889/AddClass', {data})
+.then(res =>{
+     console.log(res);
+if(res.data.insertedId){
+    alert("You have added Successfully");
+}
+     
+    
+})
+
+
+
+
+event.target.class_name.value=" ";
+event.target.Class_image.value=" ";
+
+event.target.Available_seats.value=" ";
+event.target.price.value=" ";
+
+    }
     return (
         <div className='w-full p-5'>
             <div>
                <h1 className='text-5xl font-bold my-5 text-center'>Add Course Class</h1>
             </div>
-            <form className='w-full'>
-{/* 
-            <div className="form-control w-full">
-  <label className="label">
-    <span className="label-text">Recipe Name*</span>
-  </label>
-<input type="text" placeholder="Recipe Name"name="recipe_name"  className="input input-bordered w-full" />
+            <form className='w-full'onSubmit={SubmitData}>
 
-</div> */}
 <div className='flex my-3'>
 <div className="form-control w-full ms-3">
   <label className="label">
@@ -38,14 +76,14 @@ const Add_Class = () => {
   <label className="label">
     <span className="label-text">Instructor Name*</span>
   </label>
-<input type="text" placeholder="Instructor Name"name="Instructor_Name"defaultValue={data.displayName} className="input input-bordered w-full" />
+<input type="text" placeholder="Instructor Name"name="Instructor_Name"defaultValue={data?.displayName} className="input input-bordered w-full" />
 
 </div>
 <div className="form-control w-full ms-3">
   <label className="label">
     <span className="label-text">Instructor Email*</span>
   </label>
-<input type="text" placeholder="Instructor_Email"name="Useremail"defaultValue={data.email} className="input input-bordered w-full" />
+<input type="text" placeholder="Instructor_Email"name="Useremail"defaultValue={data?.email} className="input input-bordered w-full" />
 
 </div>
 </div><div className='flex my-3'>
